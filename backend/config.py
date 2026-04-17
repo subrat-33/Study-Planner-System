b7286c5f6daa6b6e7853c806e3b70fb34dad1ed0
Subrat Mallick <subratmallick630@gmail.com>
2026-04-17 23:12:59 +0530

    Updated database configuration for deployment

--- backend/config.py
+++ backend/config.py
@@ -9,6 +9,7 @@ class Config:
     
     # MySQL Database Configuration
     MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'localhost'
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT') or 3306)
     MYSQL_USER = os.environ.get('MYSQL_USER') or 'root'
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'subratsql.in003'
     MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE') or 'study_planner'
